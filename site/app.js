19b04b8b5c3b16289cca7731672f670c05ad5fb5
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-02-23 12:42:53 -0300

    Create - Organização de Diretórios para API

---
+++ site/app.js
@@ -0,0 +1,34 @@
+process.env.AMBIENTE_PROCESSO = "desenvolvimento";
+//process.env.AMBIENTE_PROCESSO = "producao";
+
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
+
+var app = express();
+
+// Isso é uma base de outro projeto, Mudar de acordo com o grupo
+var indexRouter = require("./src/routes/index");
+var orquestraRouter = require("./src/routes/orquestra");
+var meusMusicosRouter = require("./src/routes/meusMusicos");
+var graficosRouter = require("./src/routes/graficos");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/orquestra", orquestraRouter);
+app.use("/meusMusicos", meusMusicosRouter);
+app.use("/graficos", graficosRouter);
+
+app.listen(PORTA, function () {
+    console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
+    Você está rodando sua aplicação em Ambiente de ${process.env.AMBIENTE_PROCESSO} \n
+    \t\tSe "desenvolvimento", você está se conectando ao banco LOCAL (MySQL Workbench). \n
+    \t\tSe "producao", você está se conectando ao banco REMOTO (SQL Server em nuvem Azure) \n
+    \t\t\t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'`);
+});
