a629020cd110d447076d0d9a9898116716ccbc5f
PedroRCSilva <pedro.rcsilva@sptech.school>
2023-02-27 01:05:28 -0300

    UPDATE: RESOLVENDO BUG DE ROTAS NA HORA DO NPM START

--- site/app.js
+++ site/app.js
@@ -10,9 +10,7 @@ var app = express();
 
 // Isso é uma base de outro projeto, Mudar de acordo com o grupo
 var indexRouter = require("./src/routes/index");
-var orquestraRouter = require("./src/routes/orquestra");
-var meusMusicosRouter = require("./src/routes/meusMusicos");
-var graficosRouter = require("./src/routes/graficos");
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,9 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/", indexRouter);
-app.use("/orquestra", orquestraRouter);
-app.use("/meusMusicos", meusMusicosRouter);
-app.use("/graficos", graficosRouter);
+
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
