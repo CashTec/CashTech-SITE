51897462cedcfcea8e9b4133f8affb9d75d4f222
leticia-araujo0 <leticia.araujo@sptech.school>
2023-03-24 22:56:57 -0300

    Update - Atualizando WebDataViz Login

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -2,38 +2,19 @@ var usuarioModel = require("../models/usuarioModel");
 
 var sessoes = [];
 
-function testar(req, res) {
-    console.log("ENTRAMOS NA usuarioController");
-    res.json("ESTAMOS FUNCIONANDO!");
-}
-
-function listar(req, res) {
-    usuarioModel.listar()
-        .then(function (resultado) {
-            if (resultado.length > 0) {
-                res.status(200).json(resultado);
-            } else {
-                res.status(204).send("Nenhum resultado encontrado!")
-            }
-        }).catch(
-            function (erro) {
-                console.log(erro);
-                console.log("Houve um erro ao realizar a consulta! Erro: ", erro.sqlMessage);
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-}
 
 function entrar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    console.log("Controller");
+    console.log("Email: " + email);
+    console.log("senha: " + senha);
 
     if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
-        
         usuarioModel.entrar(email, senha)
             .then(
                 function (resultado) {
@@ -96,7 +77,5 @@ function cadastrar(req, res) {
 
 module.exports = {
     entrar,
-    cadastrar,
-    listar,
-    testar
+    cadastrar
 }
