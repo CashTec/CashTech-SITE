12bf975cfb550acff14301b0a7639bd7157a47a2
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-04-01 18:10:59 -0300

    Concerto de lógica e validações com o banco

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -42,6 +42,56 @@ function entrar(req, res) {
 
 }
 
+async function cadastrar(req, res) {
+    const camposObrigatorios = [
+        'nomeEmpresaServer',
+        'cnpjServer',
+        'emailServer',
+        'telefoneServer',
+        'cepServer',
+        'numeroServer',
+        'ruaServer',
+        'cidadeServer',
+        'bairroServer',
+        'nomeResponsavelServer',
+        'loginServer',
+        'senhaServer'
+    ];
+    console.log(req.body);
+    const camposFaltantes = camposObrigatorios.filter(campo => req.body[campo] === undefined);
+
+    if (camposFaltantes.length > 0) {
+        res.status(400).send(`Os seguintes campos estão faltando: ${camposFaltantes.join(', ')}`);
+        return;
+    }
+
+    try {
+        const verificarLoginResult = await usuarioModel.verificarUsuario(req.body.loginServer, req.body.senhaServer);
+
+        if (verificarLoginResult.length >= 1) {
+            return res.status(409).json({ message: "Esse Login já existe, por favor digite outro" });
+        } else {
+            const enderecoResult = await usuarioModel.cadastrarEndereco(req.body.cepServer, req.body.numeroServer, req.body.ruaServer, req.body.cidadeServer, req.body.bairroServer);
+            const fkEndereco = enderecoResult.insertId;
+
+            const empresaResult = await usuarioModel.cadastrarEmpresa(req.body.nomeEmpresaServer, req.body.cnpjServer, req.body.emailServer, req.body.telefoneServer, fkEndereco);
+            const fkEmpresa = empresaResult.insertId;
+
+            const usuarioResult = await usuarioModel.cadastrarUsuario(req.body.nomeResponsavelServer, req.body.loginServer, req.body.senhaServer, fkEmpresa);
+
+            res.json({ enderecoResult, empresaResult, usuarioResult });
+        }
+
+    } catch (erro) {
+        console.log(erro);
+        console.log(`Houve um erro ao realizar o cadastro! Erro: ${erro.sqlMessage}`);
+        res.status(500).json(erro.sqlMessage);
+    }
+}
+
+
+/*
+Versão antiga: 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nomeEmpresa = req.body.nomeEmpresaServer;
@@ -57,19 +107,6 @@ function cadastrar(req, res) {
     var login = req.body.loginServer;
     var senha = req.body.senhaServer;
 
-    console.log("Empresa: " + nomeEmpresa);
-    console.log("CNPJ: " + cnpj);
-    console.log("Email: " + email);
-    console.log("Telefone: " + telefone);
-    console.log("CEP: " + cep);
-    console.log("Numero: " + numero);
-    console.log("Rua: " + rua);
-    console.log("Cidade: " + cidade);
-    console.log("Bairro: " + bairro);
-    console.log("Responsavel: " + nomeResponsavel);
-    console.log("Login: " + login);
-    console.log("Senha: " + senha);
-
     // Faça as validações dos valores
     if (nomeEmpresa == undefined) {
         res.status(400).send("Seu nome da empresa está undefined!");
@@ -77,44 +114,88 @@ function cadastrar(req, res) {
         res.status(400).send("Seu cnpj está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
-    } else if(telefone == undefined){
-        res.status(400).send("Seu telefone está undefined!");    
-    } else if(cep == undefined){
+    } else if (telefone == undefined) {
+        res.status(400).send("Seu telefone está undefined!");
+    } else if (cep == undefined) {
         res.status(400).send("Seu cep está undefined!");
-    }else if(numero == undefined){
+    } else if (numero == undefined) {
         res.status(400).send("Seu numero está undefined!");
-    }else if(rua == undefined){
+    } else if (rua == undefined) {
         res.status(400).send("Sua rua está undefined!");
-    }else if(cidade == undefined){
+    } else if (cidade == undefined) {
         res.status(400).send("Sua cidade está undefined!");
-    }else if(bairro == undefined){
+    } else if (bairro == undefined) {
         res.status(400).send("Seu bairro está undefined!");
-    }else if (nomeResponsavel == undefined){
+    } else if (nomeResponsavel == undefined) {
         res.status(400).send("O nome do responsavel está undefined!");
-    }else if (login == undefined){
+    } else if (login == undefined) {
         res.status(400).send("Seu login está undefined!");
-    }else if(senha == undefined){
+    } else if (senha == undefined) {
         res.status(400).send("Seu login está undefined!");
-    }else {
+    } else {
 
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nomeEmpresa, cnpj, email, telefone, cep, numero, rua, cidade, bairro, nomeResponsavel, login, senha)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
+        usuarioModel.cadastrarEndereco(cep, numero, rua, cidade, bairro).then(
+            function (resultado1) {
+                console.log("\n\nEndereco cadastrado com sucesso");
+                console.log('esse é o Id do resultado: ' + resultado1.insertId);
+                const fkEndereco = resultado1.insertId;
+
+                // ---------------------------------------
+                usuarioModel.cadastrarEmpresa(nomeEmpresa, cnpj, email, telefone, fkEndereco)
+                    .then(
+                        function (resultado2) {
+                            console.log("\n\nEmpresa cadastrada com sucesso");
+                            console.log('esse é o Id do resultado: ' + resultado2.insertId);
+                            const fkEmpresa = resultado2.insertId;
+
+                            // ---------------------------------------
+                            usuarioModel.cadastrarUsuario(nomeResponsavel, login, senha, fkEmpresa)
+                                .then(
+                                    function (resultado3) {
+                                        console.log("\n\Usuario cadastrado com sucesso");
+                                        res.json({ resultado1, resultado2, resultado3 });
+                                    }
+                                ).catch(
+                                    function (erro) {
+                                        console.log(erro);
+                                        console.log(
+                                            "\nHouve um erro ao realizar o cadastro do Usuario! Erro: ",
+                                            erro.sqlMessage
+                                        );
+                                        res.status(500).json(erro.sqlMessage);
+                                    }
+                                );
+                            // ---------------------------------------
+
+                        }
+
+                    ).catch(
+                        function (erro) {
+                            console.log(erro);
+                            console.log(
+                                "\nHouve um erro ao realizar o cadastro da Empresa! Erro: ",
+                                erro.sqlMessage
+                            );
+                            res.status(500).json(erro.sqlMessage);
+                        }
                     );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+                // -------------------------------------------------
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro do Endereco! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+
     }
 }
+*/
+
 
 module.exports = {
     entrar,
--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -1,31 +1,77 @@
 var database = require("../database/config")
 
-
 function entrar(login, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando " +
-        "corretamente. \n\n function entrar(): ", login, senha)
+    console.log("\n\n<--------------------------------------------------------------------------------------> ");
+    console.log("Model function entrar(): ", login, senha)
 
     var instrucao = `
-        SELECT * FROM usuario WHERE email = '${login}' AND senha = '${senha}';
+        SELECT * FROM usuario WHERE login = '${login}' AND senha = '${senha}';
     `;
+
     console.log("Executando a instrução SQL: \n" + instrucao);
+
     return database.executar(instrucao);
 }
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrar(nomeEmpresa, cnpj, email, telefone, cep, numero, rua, cidade, bairro, nomeResponsavel, login, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nomeEmpresa, cnpj, email, telefone, cep, numero, rua, cidade, bairro, nomeResponsavel, login, senha);
+function verificarUsuario(login, senha) {
+    console.log("\n\n<--------------------------------------------------------------------------------------> ");
+    console.log("Model function verificarLogin(): ", login, senha)
 
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nomeEmpresa, cnpj, email, telefone, cep, numero, rua, cidade, bairro, nomeResponsavel, login, senha) VALUES ('${nomeEmpresa}', '${cnpj}', '${email}', '${telefone}', '${cep}', '${numero}', '${rua}', '${cidade}', '${bairro}', '${nomeResponsavel}', '${login}', '${senha}');
+        SELECT * FROM usuario WHERE login = '${login}' AND senha = '${senha}';
     `;
+
     console.log("Executando a instrução SQL: \n" + instrucao);
+
     return database.executar(instrucao);
 }
+
+function cadastrarEmpresa(nomeEmpresa, cnpj, email, telefone, fkEndereco) {
+    console.log("\n\n<--------------------------------------------------------------------------------------> ");
+    console.log(`Model function cadastrarEmpresa():`, nomeEmpresa, cnpj, email, telefone, fkEndereco);
+
+    var instrucao = `
+        INSERT INTO empresa (nome, cnpj, email, telefone, fkEndereco) VALUES ('${nomeEmpresa}', '${cnpj}', '${email}', '${telefone}',${fkEndereco});
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucao);
+
+    return database.executar(instrucao);
+}
+
+
+function cadastrarEndereco(cep, numero, rua, cidade, bairro) {
+    console.log("\n\n<--------------------------------------------------------------------------------------> ");
+    console.log(`Model function cadastrarEndereco():`, cep, numero, rua, cidade, bairro);
+
+    var instrucao = `
+        INSERT INTO endereco (cep, numero, rua, cidade, bairro) VALUES ('${cep}', '${numero}', '${rua}', '${cidade}', '${bairro}');
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucao);
+
+    return database.executar(instrucao);
+}
+
+
+function cadastrarUsuario(nomeResponsavel, login, senha, fkEmpresa) {
+    console.log("\n\n<--------------------------------------------------------------------------------------> ");
+    console.log(`Model function cadastrarUsuario():`, nomeResponsavel, login, senha, fkEmpresa);
+
+    var instrucao = `
+        INSERT INTO usuario (nome, login, senha, fkEmpresa) VALUES ('${nomeResponsavel}', '${login}', '${senha}',${fkEmpresa});
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucao);
+
+    return database.executar(instrucao);
+}
+
 
 module.exports = {
     entrar,
-    cadastrar
+    cadastrarEmpresa,
+    cadastrarEndereco,
+    cadastrarUsuario,
+    verificarUsuario
 };
