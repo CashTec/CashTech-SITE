8ed8b386482b5908d89574c152819039d6428fd6
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-04-01 18:10:24 -0300

    Script para teste

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,99 +1,43 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database cashtech;
+use cashtech;
+CREATE TABLE IF NOT EXISTS Endereco (
+  `idEndereco` INT NOT NULL auto_increment,
+  `rua` VARCHAR(45) NULL,
+  `bairro` VARCHAR(45) NULL,
+  cidade varchar(45) null,
+  `numero` VARCHAR(10) NULL,
+  `cep` CHAR(8) NULL,
+  PRIMARY KEY (`idEndereco`))
+ENGINE = InnoDB;
 
-/*
-comandos para mysql - banco local - ambiente de desenvolvimento
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-
-/*
-comando para sql server - banco remoto - ambiente de produção
-*/
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY IDENTITY(1,1),
-	descricao VARCHAR(300)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
-);
-
-/*
-comandos para criar usuário em banco de dados azure, sqlserver,
-com permissão de insert + update + delete + select
-*/
-
-CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
-WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
-DEFAULT_SCHEMA = dbo;
-
-EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
-
-EXEC sys.sp_addrolemember @rolename = N'db_datareader',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+CREATE TABLE IF NOT EXISTS Empresa (
+  `idEmpresa` INT NOT NULL auto_increment,
+  `nome` VARCHAR(45) NOT NULL,
+  cnpj CHAR(14) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `telefone` CHAR(11) NOT NULL,
+  `fkEndereco` INT NOT NULL,
+  PRIMARY KEY (`idEmpresa`),
+  INDEX `fk_Empresa_Endereco_idx` (`fkEndereco` ASC) VISIBLE,
+  CONSTRAINT `fk_Empresa_Endereco`
+    FOREIGN KEY (`fkEndereco`)
+    REFERENCES `Endereco` (`idEndereco`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS Usuario (
+  `idUsuario` INT NOT NULL auto_increment,
+  `nome` VARCHAR(45) NULL,
+  `login` VARCHAR(45) NULL unique,
+  `senha` VARCHAR(45) NULL,
+  `tipoUsuario` VARCHAR(45) NULL DEFAULT 'admin',
+  `fkEmpresa` INT NOT NULL,
+  PRIMARY KEY (`idUsuario`, `fkEmpresa`),
+  INDEX `fk_Usuario_Empresa1_idx` (`fkEmpresa` ASC) VISIBLE,
+  CONSTRAINT `fk_Usuario_Empresa1`
+    FOREIGN KEY (`fkEmpresa`)
+    REFERENCES `Empresa` (`idEmpresa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+select * from usuario;
