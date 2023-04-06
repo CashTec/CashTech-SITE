1ad3a5a91867e6bd383cee8f8d5163d89fc5a3ee
PedroRCSilva <pedro.rsilva@sptech.school>
2023-04-06 10:05:02 -0300

    inserindo graficos na dashboard geral

--- site/public/js/dashboard-geral.js
+++ site/public/js/dashboard-geral.js
@@ -1,48 +1,54 @@
-const graphicLine = document.getElementById('graphicLine');
+const graphicLine = document.getElementById("graphicLine");
 
-  new Chart(graphicLine, {
-    type: 'line',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
+new Chart(graphicLine, {
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
         data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-
-  const graphicBola = document.getElementById('graphicBola');
-
-  new Chart(graphicBola, {
-    type: 'doughnut',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+        borderWidth: 1,
       },
-      elements:{
-        arc:{
-          spacing:40
-        },
-        borderWidth:2 
-      }
-    }
-  });
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
 
+let data = [12, 19, 3, 5, 2, 3];
+
+const graphicBola = document.getElementById("graphicBola");
+const config = {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        position: "bottom", 
+        labels:{
+          usePointStyle: true,
+          padding:30,
+          color:"#222"
+          }
+        },
+        
+      },
+    },
+  }
+
+new Chart(graphicBola, config);
