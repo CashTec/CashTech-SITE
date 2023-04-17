7fe899d06b8fe6bdcafab35bf0e2ee7d4acc1fde
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-04-16 22:18:17 -0300

    UPDATE - atualizações e correções de bugs

--- site/public/js/apiCEP.js
+++ site/public/js/apiCEP.js
@@ -1,16 +1,35 @@
-var cep = input_CepCadastro;
-var logradouro = input_ruaCadastro;
-var bairro = input_bairroCadastro;
-var cidade = input_cidadeCadastro;
+var cep;
+var logradouro;
+var bairro;
+var cidade;
 
 function pegarEndereco() {
+    cep = input_CepCadastro;
+    logradouro = input_ruaCadastro;
+    bairro = input_bairroCadastro;
+    cidade = input_cidadeCadastro;
+
     var script = document.createElement('script');
 
     script.src = `https://viacep.com.br/ws/${cep.value}/json/?callback=retornarEndereco`;
 
     document.body.appendChild(script);
 }
- 
+
+
+function pegarEnderecoModal() {
+    cep = in_edtCep;
+    logradouro = in_edtRua;
+    bairro = in_edtBairro;
+    cidade = in_edtCidade;
+
+    var script = document.createElement('script');
+
+    script.src = `https://viacep.com.br/ws/${cep.value}/json/?callback=retornarEndereco`;
+
+    document.body.appendChild(script);
+}
+
 function retornarEndereco(endereco) {
     logradouro.value = (endereco.logradouro);
     bairro.value = (endereco.bairro);
