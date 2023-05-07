6ed8af4d44593a376f88b24bd830d472c18a9d5d
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-05-07 16:04:20 -0300

    animação para fazer login e verificar

--- site/public/js/login.js
+++ site/public/js/login.js
@@ -1,4 +1,8 @@
+let isLoading = false;
+
 function entrar() {
+    spn_login.style.display = 'none';
+    loadingGif.style.display = 'flex';
 
     var emailVar = document.getElementById("input_emailLogin").value;
     var senhaVar = document.getElementById("input_senhaLogin").value;
@@ -32,19 +36,24 @@ function entrar() {
 
             });
 
-            window.location.pathname = "dashboard-geral.html"
+            window.location.pathname = "mapa-gestor.html"
 
         } else {
 
+            spn_invalido.style.display = 'block';
             console.log("Houve um erro ao tentar realizar o login!");
-            
+
             resposta.text().then(texto => {
                 console.error(texto);
             });
         }
-
+        spn_login.style.display = 'flex';
+        loadingGif.style.display = 'none';
+    
     }).catch(function (erro) {
         console.log(erro);
+        isLoading = false;
+
     })
 
     return false;
